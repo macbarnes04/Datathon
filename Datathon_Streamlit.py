6ad09419021d9aefd5430809e79cc3afe6c2fb61
Ganning Xu <ganningxu@gmail.com>
2022-04-05 02:00:10 -0400

    added select and stats

--- Datathon_Streamlit.py
+++ Datathon_Streamlit.py
@@ -4,23 +4,135 @@ import folium
 from streamlit_folium import folium_static
 import pandas as pd
 import geopandas as gpd
+from annotated_text import annotated_text
 
+# READING IN DATA
 df = pd.read_csv("broadband_survey.csv")
-df = df.loc[df["state_code"] == 37]
-primaryColor = "#FF8B3D"
-st.markdown("<h1 style='text-align: center; color: black;"
-            "'>Tracking the NC Digital Divide: A Report Card Dashboard 📄 </h1>",
-            unsafe_allow_html=True)
-st.title("")
+nc_counties = gpd.read_file('NC Counties2.geojson')
+nc_data = pd.read_csv("county_data.csv")
 
-st.sidebar.title("Choose A County:")
+# DATA PREPROCESSING
+df = df.loc[df["state_code"] == 37]  # only NC
 
-page = st.sidebar.selectbox(
-    '',
-    df["county_name"].value_counts().index.to_list(),
+nc_data["joiner"] = nc_data["county_name"].str.split(
+    pat=" County", expand=True)[0]
+nc_data["joiner"] = nc_data["joiner"].str.upper()
+# join assignment csv to precinct shapefile
+nc_counties_join = nc_counties.merge(
+    nc_data, left_on='CO_NAME', right_on='joiner', how='left')
+# geographic dissolve of precincts based on assignment column
+nc_counties_dissolve = nc_counties_join.dissolve(by='county_name')
+nc_counties_dissolve['county_name'] = nc_counties_dissolve.index.copy()
+null_columns = nc_counties_dissolve.columns[nc_counties_dissolve.isnull(
+).any()]
+
+
+# create mapper from full name to abbreviation
+full_county_names = list(df["county_name"].unique())
+
+abbr_county_names = list(
+    nc_counties_join["CO_NAME"].unique())
+
+full_to_abbr_names = {'All': 'All'}
+for full_name in full_county_names:
+    for abbr_name in abbr_county_names:
+        if full_name.split()[0].lower() == abbr_name.lower():
+            full_to_abbr_names[full_name] = abbr_name
+
+st.sidebar.title("Choose your metrics:")
+
+selectbox_options = ["All", *df["county_name"].value_counts().index.to_list()]
+full_county = st.sidebar.selectbox(
+    'County Name',
+    selectbox_options,
 )
 
-st.sidebar.button("Generate Report Card", on_click=None)
+# User options
+abbr_chosen = full_to_abbr_names[full_county]
+
+# PAGE FORMATTING
+# st.markdown("<h1 style='text-align: center; color: white;"
+#             "'>Tracking the NC Digital Divide: A Report Card Dashboard 📄 </h1>",
+#             unsafe_allow_html=True)
+
+
+m = folium.Map(location=[35.6516, -79.92], tiles="CartoDB positron",
+               name="Light Map", zoom_start=7, attr="My Data Attribution")
+
+textColor = "#262730"
+
+nc_avg_per_access = round(nc_counties_join['per_access'].mean() * 100, 2)
+data_using = None
+
+colors = {
+    "lower than average": "#ff7a7a",
+    "average": "#ffda33",
+    "higher than average": "#00f070",
+}
+if abbr_chosen == "All":
+    st.markdown("# Tracking the NC Digital Divide: A Report Card Dashboard 📄")
+    # find the average of the column per_access in nc_counties_join
+
+    # st.markdown("#### Overall, " + str(nc_avg_per_access) +
+    #             "% of people have access to broadband")
+    annotated_text(
+        "The average percentage of people in North Carolina who have access to broadband is ",
+        (str(nc_avg_per_access) + "%", "low", "#ff7a7a"),
+    )
+
+    # annotated_text(
+    #     "The average percentage of people in the North Carolina state have access to broadband is ",
+    #     (str(nc_avg_per_access) + "%", "low", "#ff7a7a"),
+    # )
+
+    data_using = nc_counties_join
+else:
+    data_using = nc_counties_join.loc[nc_counties_join["CO_NAME"]
+                                      == abbr_chosen]
+
+    spec_per = round(
+        float(data_using["per_access"].to_string().split()[1]) * 100, 2)
+    st.markdown("# " + full_county + "'s grade report 📄")
+    # per_access_string = "#### " + \
+    #     str(spec_per) + "% have access to broadband internet"
+    # st.write(per_access_string)
+    level = None
+    if spec_per < nc_avg_per_access:
+        level = "lower than average"
+    elif spec_per > nc_avg_per_access:
+        level = "higher than average"
+    else:
+        level = "average"
+    annotated_text(
+        "There are ",
+        (str(spec_per) + "%", level, colors[level]),
+        " of people in " + full_county + " who have access to broadband internet",
+    )
+# folium.Marker(
+#     location=[34.9790, -79.2461],
+#     popup="Mt. Hood Meadows",
+#     icon=folium.Icon(icon="cloud"),
+# ).add_to(m)
+
+st.write("")
+folium.Choropleth(
+    geo_data=data_using,
+    name="choropleth",
+    data=data_using,
+    columns=["county_code", "per_access"],
+    key_on='feature.properties.county_code',
+    fill_color="Purples",
+    fill_opacity=1.0,
+    line_opacity=0.4,
+    legend_name='25/3 Broadband Access (% by County)',
+).add_to(m)
+
+# st.write(nc_data)
+
+
+folium.LayerControl().add_to(m)
+folium_static(m, width=850, height=500)
+m
 
 
 # DEFINITIONS
@@ -81,50 +193,3 @@ st.sidebar.button("Generate Report Card", on_click=None)
 #             unsafe_allow_html=True)
 #
 # read in precinct shapefile
-nc_counties = gpd.read_file('NC Counties2.geojson')
-# 25/3 data
-nc_data = pd.read_csv("county_data.csv")
-# read in assignment file
-# check for unique ID column to match length of precinct df
-# nc_counties['loc_prec'].nunique()
-# len(nc_precs)
-
-
-nc_data["joiner"] = nc_data["county_name"].str.split(pat=" County",expand=True)[0]
-nc_data["joiner"] = nc_data["joiner"].str.upper()
-# join assignment csv to precinct shapefile
-nc_counties_join = nc_counties.merge(nc_data, left_on='CO_NAME', right_on='joiner', how='left')
-# geographic dissolve of precincts based on assignment column
-nc_counties_dissolve = nc_counties_join.dissolve(by='county_name')
-nc_counties_dissolve['county_name'] = nc_counties_dissolve.index.copy()
-null_columns = nc_counties_dissolve.columns[nc_counties_dissolve.isnull().any()]
-
-#
-# nc_precs_dissolve.to_file('r"/Users/macbarnes/Desktop/Data/nc_precs_dissolve.geojson"', driver='GeoJSON')
-#
-# nc_precs_dissolve.to_file('r"/Users/macbarnes/Desktop/NC_precs/nc_precs_dissolve.shp"')
-
-
-m = folium.Map(location=[35.6516, -79.92], tiles="CartoDB positron", name="Light Map",
-               zoom_start=7, attr="My Data Attribution")
-folium.Choropleth(
-    geo_data=nc_counties_join,
-    name="choropleth",
-    data=nc_counties_join,
-    columns=["county_code", "per_access"],
-    key_on='feature.properties.county_code',
-    fill_color="Purples",
-    fill_opacity=0.7,
-    line_opacity=0.1,
-    legend_name='25/3 Broadband Access (% by County)',
-).add_to(m)
-
-
-folium.LayerControl().add_to(m)
-folium_static(m, width=850, height=500)
-m
-
-
-
-
-
